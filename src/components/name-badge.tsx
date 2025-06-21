ef5a76b84935baad5a2f64ec4f4e44968cd118ef
Gregg Trunnell <gggtrunnell@gmail.com>
2025-06-21 15:56:56 -0500

    added to name-badge and application comps

--- src/components/name-badge.tsx
+++ src/components/name-badge.tsx
@@ -1,4 +1,8 @@
-const NameBadge = () => {
+//const NameBadge = ({name}:{name: string}) => {} could also be written
+//like: const addTwo =(n: number)=> n + 2 changing it to...
+
+const NameBadge = (props:{name: string}) => {
+  if(Math.random()) return null;
   return (
     <section className="badge">
       <header className="badge-header">
@@ -6,7 +10,7 @@ const NameBadge = () => {
         <p>My name is…</p>
       </header>
       <div className="badge-body">
-        <p className="badge-name">Triple G</p>
+        <p className="badge-name">{props.name}</p>
       </div>
       <footer className="badge-footer" />
     </section>
